8285445b45ee263608a770c32753e69bdddcaa3b
Shinnanda0707 <choihyun0723@naver.com>
2024-07-01 13:26:19 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import physics_engine as pe
+import pymunk
+
+space = pymunk.Space()
+space.gravity = (0, -981)
+
+obj = pymunk.Body()
+obj.position = (0, 0)
+
+obj_poly = pymunk.Poly.create_box(obj)
+obj_poly.mass = 10
+space.add(obj, obj_poly)
+
+print_option = pymunk.SpaceDebugDrawOptions()
+
+for _ in range(100):
+    space.step(0.02)
+    space.debug_draw(print_option)
