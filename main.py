8dbc5b7f07ad088ee4b697525dee180ee667c7e7
Shinnanda0707 <choihyun0723@naver.com>
2024-07-01 15:57:49 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ pygame.init()
 
 # Define object class
 class Object():
-    def __init__(self, size: tuple, position: tuple, color: tuple) -> None:
+    def __init__(self, mass, size: tuple, position: tuple, color: tuple, space) -> None:
+        self.mass = mass
         self.size_x, self.size_y = size
         self.x, self.y = position
         self.color = color
+
+        self.object_var = pymunk.Body(self.mass)
+        self.object_var.position = (self.x, self.y)
+        
+        self.poly = pymunk.Poly.create_box(self.object_var)
+        self.poly.mass = self.mass
+
+        space.add(self.object_var, self.poly)
+
+    def update(self):
+        self.x, self.y = self.object_var.position
     
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.size_x, self.size_y))
@@ -19,13 +31,6 @@ class Object():
 # Set pymunk variables
 space = pymunk.Space()
 space.gravity = (0, 10000)
-
-obj = pymunk.Body()
-obj.position = (50, 0)
-
-obj_poly = pymunk.Poly.create_box(obj)
-obj_poly.mass = 10
-space.add(obj, obj_poly)
 
 # Set pygame variables
 WHITE = (255, 255, 255)
@@ -38,7 +43,7 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Simulation")
 
 # Declare objects
-obj1 = Object((10, 10), tuple(obj.position), BLUE)
+objs = [Object(10, (20, 20), (250, 0), BLUE, space)]
 
 # Main loop
 for _ in range(100):
@@ -47,11 +52,10 @@ for _ in range(100):
     # Initialize window
     win.fill(WHITE)
 
-    # Update object position
-    obj1.x, obj1.y = obj.position
-    
-    # Draw object
-    obj1.draw(win)
+    # Update object position and draw
+    for o in objs:
+        o.update()
+        o.draw(win)
 
     pygame.display.update()
 
