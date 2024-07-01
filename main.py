f0b9fdc5b03ebd852bfd935369ffcd1ddabcef18
Shinnanda0707 <choihyun0723@naver.com>
2024-07-01 17:23:15 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-
 import pygame
 import pymunk
+import pymunk.pygame_util
 pygame.init()
 
 
@@ -21,16 +20,6 @@ class Object():
 
         space.add(self.object_var, self.poly)
 
-    def update(self):
-        self.x, self.y = self.object_var.position
-    
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.size_x, self.size_y))
-
-
-# Set pymunk variables
-space = pymunk.Space()
-space.gravity = (0, 10000)
 
 # Set pygame variables
 WHITE = (255, 255, 255)
@@ -39,27 +28,34 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+fps = 50
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Simulation")
+clock = pygame.time.Clock()
+run = True
+
+# Set pymunk variables
+space = pymunk.Space()
+space.gravity = (0, 900)
+draw_options = pymunk.pygame_util.DrawOptions(win)
 
 # Declare objects
 objs = [Object(10, (20, 20), (250, 0), BLUE, space)]
 
 # Main loop
-for _ in range(100):
-    space.step(0.01)
+while run:
+    # Update simulation
+    space.step(1 / fps)
+    clock.tick(fps)
 
     # Initialize window
     win.fill(WHITE)
-
-    # Update object position and draw
-    for o in objs:
-        o.update()
-        o.draw(win)
+    space.debug_draw(draw_options)
 
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-    time.sleep(0.1)
+            run = False
+            break
+pygame.quit()
