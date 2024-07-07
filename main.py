f074b856a7f2441664c4010d9332568a2353edaf
Shinnanda0707 <choihyun0723@naver.com>
2024-07-07 23:45:02 +0900

    modified:   main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+from random import choice
+
 import pygame
 import pymunk
 import pymunk.pygame_util
@@ -43,6 +46,22 @@ def create_guide_line(window, start_pos: tuple, line_started: bool) -> None:
         pygame.draw.line(window, (0, 255, 0), start_pos, pygame.mouse.get_pos(), 2)
 
 
+def visualize_velocity(window, balls, font, scale_factor: float) -> None:
+    for ball in balls:
+        # Get velocity and position of each ball
+        vel_x, vel_y = ball.object_var.velocity
+        pos_x, pos_y = ball.object_var.position
+
+        # Write mass and velocity
+        mass_info = font.render(f"mass: {ball.mass}", True, (0, 0, 0))
+        vel_info = font.render(f"vel: {round(math.sqrt(math.pow(vel_x, 2) + math.pow(vel_y, 2)), 2)}", True, (0, 0, 0))
+        window.blit(mass_info, (pos_x - 10, pos_y - 70))
+        window.blit(vel_info, (pos_x - 10, pos_y - 50))
+
+        # Make guideline pointing ball's moving direction
+        pygame.draw.line(window, (225, 0, 0), (pos_x, pos_y), (pos_x + vel_x * scale_factor, pos_y + vel_y * scale_factor))
+
+
 # Define main loop
 def run(win, space, fps: int, balls: list) -> None:
     create_boundaries(space, 700, 700)
@@ -52,6 +71,8 @@ def run(win, space, fps: int, balls: list) -> None:
     line_started = False
     line_start_pos = (0, 0)
     info = font.render("ball(b), line(l)", False, (0, 0, 0))
+    force = (0, 0)
+    force_cnt = 0
 
     while run:
         # Update simulation
@@ -63,12 +84,15 @@ def run(win, space, fps: int, balls: list) -> None:
         space.debug_draw(draw_options)
 
         # Show mode on window
-        surface = font.render(f"Mode: {mode}", False, (0, 0, 0))
+        surface = font.render(f"Mode: {mode}", True, (0, 0, 0))
         win.blit(surface, (10, 10))
         win.blit(info, (10, 30))
 
         # Create line when the variable mode is "line" and line was started
         create_guide_line(win, line_start_pos, line_started)
+
+        # Show velocity and mass above balls + draw line toward balls' moving direction
+        visualize_velocity(win, balls, font, 0.1)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -90,7 +114,8 @@ def run(win, space, fps: int, balls: list) -> None:
                 if bt_left:
                     # Ball falling mode
                     if mode == "ball":
-                        balls.append(Object(10, 20, pygame.mouse.get_pos(), (0, 0, 0, 100), space))
+                        for _ in range(100):
+                            balls.append(Object(10, 10, pygame.mouse.get_pos(), (0, 0, 0, 100), space))
                     
                     # Line drawing mode
                     elif mode == "line":
@@ -99,15 +124,18 @@ def run(win, space, fps: int, balls: list) -> None:
                             line_start_pos = pygame.mouse.get_pos()
                         else:
                             line_started = False
-
                             body = pymunk.Body(body_type=pymunk.Body.STATIC)
-                            shape = pymunk.Segment(body, line_start_pos, tuple(pygame.mouse.get_pos()), 2)
+                            shape = pymunk.Segment(body, line_start_pos, tuple(pygame.mouse.get_pos()), 0)
+                            shape.friction = 0
                             space.add(body, shape)
                 
                 # Apply impulse force to the object
                 if bt_right:
                     for ball in balls:
-                        ball.poly.body.apply_impulse_at_local_point((6000, 0), pygame.mouse.get_pos())
+                        f_x, f_y = choice([50, -50]), -500
+                        v_x, v_y = ball.object_var.velocity
+                        time = 7
+                        ball.object_var.velocity = v_x + time * f_x / ball.mass, v_y + time * f_y / ball.mass
         
         # Update window
         pygame.display.update()
