ce1573078c22da2417fec61f9a182fa3a7be9bc9
박성은(SeongeunPark) <92924028+SEONGEUN0707@users.noreply.github.com>
2024-07-09 15:25:05 +0900

    Update 현라에몽 도와줘.py

--- 현라에몽 도와줘.py
+++ 현라에몽 도와줘.py
@@ -19,17 +19,24 @@ def calculate_distance(p1,p2):
 def calculate_angle(p1,p2):
     return math.atan2(p2[1] - p1[1] , p2[0] - p1[0])
 
-def visualize_velocity(window, balls, font, scale_factor: float) -> None:
+def visualize_velocity(window, balls, font, scale_factor: float, is_pause) -> None:
     for ball in balls:
         # Get velocity and position of each ball
-        vel_x, vel_y = ball.body.velocity
-        pos_x, pos_y = ball.body.position
+        vel_x, vel_y = ball[0].body.velocity
+        pos_x, pos_y = ball[0].body.position
 
         # Write mass and velocity
-        mass_info = font.render(f"mass: {ball.mass}", True, (0, 0, 0))
-        vel_info = font.render(f"vel: {round(math.sqrt(math.pow(vel_x, 2) + math.pow(vel_y, 2)), 2)}", True, (0, 0, 0))
+        mass_info = font.render(f"mass: {ball[0].mass}", True, (0, 0, 0))
+        if is_pause:
+            vel_info = font.render(f"vel: {round(math.sqrt(math.pow(vel_x, 2) + math.pow(vel_y, 2)), 2)}", True, (0, 0, 0))
+            window.blit(vel_info, (pos_x - 10, pos_y - 50))
+        else:
+            if ball[1]:
+                vel_x, vel_y = ball[1]
+                vel_info = font.render(f"vel: {round(math.sqrt(math.pow(vel_x, 2) + math.pow(vel_y, 2)), 2)}", True, (0, 0, 0))
+                window.blit(vel_info, (pos_x - 10, pos_y - 50))
         window.blit(mass_info, (pos_x - 10, pos_y - 70))
-        window.blit(vel_info, (pos_x - 10, pos_y - 50))
+        
 
         # Make guideline pointing ball's moving direction
         pygame.draw.line(window, (225, 0, 0), (pos_x, pos_y), (pos_x + vel_x * scale_factor, pos_y + vel_y * scale_factor))
@@ -65,7 +72,7 @@ def create_boundaries(space, width, height):
         body = pymunk.Body(body_type=pymunk.Body.STATIC)
         body.position = pos
         shape = pymunk.Poly.create_box(body, size)
-        shape.elasticity = 0
+        shape.elasticity = 0.1
         shape.friction = 0
         space.add(body, shape)
 
@@ -78,11 +85,14 @@ def create_Ball(space, radius, mass, pos, balls, is_pause):
     body.position = pos
     shape = pymunk.Circle(body, radius)
     shape.mass = mass
-    shape.elasticity = 0.9
-    shape.color = (255, 0 ,0, 100)
+    shape.elasticity = 0
+    shape.color = (199, 199 , 199, 100)
     space.add(body, shape)
-    balls.append(shape)
+    balls.append([shape, None])
     return shape, balls
+
+def shoot_balls():
+    pass
 
 #main
 def run(window, width, height):
@@ -91,6 +101,7 @@ def run(window, width, height):
 
     line = None
     first_pos = None
+    pressed_pos = None
     is_pause = False
     mode = 1
 
@@ -108,10 +119,12 @@ def run(window, width, height):
     
     while run:
 
-        if mode % 2 == 1:
+        if mode % 3 == 1:
             Text = Font.render("Mode : Create_Ball", True, (34,139,34))
-        elif mode % 2 == 0:
+        elif mode % 3 == 2:
             Text = Font.render("Mode : Draw_Lines", True, (34,139,34))
+        elif mode % 3 == 0:
+            Text = Font.render("Mode : Shoot_Ball", True, (34,139,34))
         
         if is_pause:
             Text_1 = Font.render("STATUS : MOVE", True, (34,139,34))
@@ -120,10 +133,19 @@ def run(window, width, height):
         
         window.blit(Text, (10,10))
         window.blit(Text_1, (10,30))
-
-        visualize_velocity(window, balls, Font, 0.1)
+        visualize_velocity(window, balls, Font, 0.1, is_pause)
         
         pygame.display.update()
+
+        #Ball 감지
+        mouse_pos = pymunk.pygame_util.get_mouse_pos(window)
+        near_shape = space.point_query_nearest(
+            mouse_pos, float("inf"), pymunk.ShapeFilter()
+        ).shape
+        print(near_shape)
+        if near_shape is not None and isinstance(near_shape, pymunk.Circle):
+            r = near_shape.radius + 4
+            pygame.draw.circle(window, pygame.Color("red"), near_shape.body.position, int(r), 2)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -131,24 +153,32 @@ def run(window, width, height):
                 break
 
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if mode % 2 == 0:
+                if mode % 3 == 1:
+                    create_Ball(space,30,10,pygame.mouse.get_pos(),balls, is_pause)
+
+                elif mode % 3 == 2:
                     if not first_pos:
                         first_pos = pygame.mouse.get_pos()
                     else:
                         line = [first_pos, pygame.mouse.get_pos()]
                         first_pos = None
-                else:
-                    create_Ball(space,30,10,pygame.mouse.get_pos(),balls, is_pause)
+                
+                elif mode % 3 == 0:
+                    pass
+                
 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
                     if is_pause:
                         for ball in balls:
-                            ball.body.body_type = pymunk.Body.STATIC
+                            ball[1] = ball[0].body.velocity
+                            ball[0].body.body_type = pymunk.Body.STATIC
                         is_pause = False
                     else:
                         for ball in balls:
-                            ball.body.body_type = pymunk.Body.DYNAMIC
+                            ball[0].body.body_type = pymunk.Body.DYNAMIC
+                            if ball[1]:
+                                ball[0].body.velocity = ball[1]
                         is_pause = True
 
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
